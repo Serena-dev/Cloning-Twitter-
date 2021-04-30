9ce34e27078470d4c3be4c227175c9de2188db42
Serena-dev <serenachoiiiiiii@gmail.com>
2021-04-30 17:19:01 +0900

    Test

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC90UfjH5GpeixgT6aK9hY3U634kqrjgVw",
+    authDomain: "ntwitter-34111.firebaseapp.com",
+    projectId: "ntwitter-34111",
+    storageBucket: "ntwitter-34111.appspot.com",
+    messagingSenderId: "701539920811",
+    appId: "1:701539920811:web:7c8e25172b11b5091900ee"
+  };
+
+export default firebase.initializeApp(firebaseConfig);
